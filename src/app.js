affead912ce5b18f94af6bf2bf9ce144eeb04d9b
PorrasD <122325175+PorrasD@users.noreply.github.com>
2024-11-19 04:46:41 +0000

    Domain Generator Finished

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function currentDomain() {
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".us", ".io"];
+
+  let all_domains = [];
+
+  for (let current_pronoun of pronouns) {
+    for (let current_adj of adjs) {
+      for (let current_noun of nouns) {
+        for (let current_extension of extensions) {
+          let new_domain = `${current_pronoun}${current_adj}${current_noun}${current_extension}`;
+          all_domains.push(new_domain);
+        }
+      }
+    }
+  }
+
+  let indice = Math.floor(Math.random() * all_domains.length);
+  return all_domains[indice];
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#showedDomain").textContent = currentDomain();
+  });
 };
